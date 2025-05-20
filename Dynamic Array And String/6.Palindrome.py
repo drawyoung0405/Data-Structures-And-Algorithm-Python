32f33a4b0102b5bcccf5afa10b23a19298bf8832
Vo Nhu Hoa <hoavn0934813785@gmail.com>
2025-05-20 09:14:40 +0700

    palindrome

---
+++ Dynamic Array And String/6.Palindrome.py
@@ -0,0 +1,16 @@
+print("Enter target:")
+x = int(input())
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x!= 0 and x % 10 ==0):
+            return False
+        half = 0
+        while x > half:
+            half = (half * 10) + (x % 10)
+            x = x // 10
+        return x == half or x == half // 10
+        solution = Solution()
+        if solution.isPalindrome(x):
+            print('True')
+        else:
+            print('False')
