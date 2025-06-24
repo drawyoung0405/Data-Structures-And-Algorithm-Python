d2e078efa692741628bb037ad9f4da6e06c840fa
Vo Nhu Hoa <hoavn0934813785@gmail.com>
2025-06-24 16:14:28 +0700

    done Longest Commom Prefix

--- Dynamic Array And String/7. AddTwoNumbers.py
+++ Dynamic Array And String/7. AddTwoNumbers.py
@@ -21,3 +21,27 @@ class Solution:
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         return dummy.next
+def to_linked_list(num_str):
+    dummy = ListNode()
+    current = dummy
+    for ch in reversed(num_str):  # Reverse input like 243 → [3 → 4 → 2]
+        current.next = ListNode(int(ch))
+        current = current.next
+    return dummy.next
+def print_linked_list(node):
+    while node:
+        print(node.val, end= "")
+        node = node.next
+    print("None")
+if __name__ == "__main__":
+    num1 = input("Enter first number: ")  # Example: 243
+    num2 = input("Enter second number: ")  # Example: 564
+
+    l1 = to_linked_list(num1)
+    l2 = to_linked_list(num2)
+
+    sol = Solution()
+    result = sol.addTwoNumbers(l1, l2)
+
+    print("Result Linked List:")
+    print_linked_list(result)
