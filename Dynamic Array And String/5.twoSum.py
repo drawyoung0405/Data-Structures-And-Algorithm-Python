9e4c8cfad29e4bb314671cbb03e8281bf1644319
drawyoung0405 <hoavn0934813785@gmail.com>
2026-02-01 00:12:27 +0700

    update input of array

--- Dynamic Array And String/5.twoSum.py
+++ Dynamic Array And String/5.twoSum.py
@@ -3,8 +3,13 @@ from typing import List
 print("Enter target")
 target = int(input())
 print("Enter Array")
-nums = list(map(int, input().split()))
-
+raw_input = input()
+if ' ' in raw_input:
+    # Xử lý chuỗi số có dấu cách, ví dụ: "1 2 3"
+    nums = list(map(int, raw_input.split()))
+else:
+    # Xử lý chuỗi số không có dấu cách, ví dụ: "123"
+    nums = [int(char) for char in raw_input]
 
 def twoSum(nums: List[int], target) -> List[int]:
     num_store = {}
